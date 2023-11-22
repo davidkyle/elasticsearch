2b124ba1e4049c02d9ca2dd09108ad8d27b34d92
David Kyle <david.kyle@elastic.co>
2023-11-22 16:49:01 +0000

    Add tests

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elser/ElserMlNodeService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elser/ElserMlNodeService.java
@@ -156,7 +156,12 @@ public class ElserMlNodeService implements InferenceService {
     }
 
     @Override
-    public void infer(Model model, List<String> input, Map<String, Object> taskSettings, ActionListener<List<? extends InferenceResults>> listener) {
+    public void infer(
+        Model model,
+        List<String> input,
+        Map<String, Object> taskSettings,
+        ActionListener<List<? extends InferenceResults>> listener
+    ) {
         // No task settings to override with requestTaskSettings
 
         if (model.getConfigurations().getTaskType() != TaskType.SPARSE_EMBEDDING) {
